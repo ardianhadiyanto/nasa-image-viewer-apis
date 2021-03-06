00c5915dd3570622cb250bc923c5567f16ead357
Ardian Hadiyanto <ardianhadiyanto@gmail.com>
2021-03-06 16:34:26 -0600

    Initial project bootstrap.

---
+++ src/main/java/io/resi/apis/NasaImageViewerApisApplication.java
@@ -0,0 +1,13 @@
+package io.resi.apis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NasaImageViewerApisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NasaImageViewerApisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/resi/apis/NasaImageViewerApisApplicationTests.java
@@ -0,0 +1,13 @@
+package io.resi.apis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NasaImageViewerApisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
